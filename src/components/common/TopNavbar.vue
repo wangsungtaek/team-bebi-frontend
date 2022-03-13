3157e768f941fb423d3d311fa1235cfcfef05024
wangsungtaek <tjdxorv@naver.com>
2022-03-14 07:53:59 +0900

    header 다시 제작중...

---
+++ src/components/common/TopNavbar.vue
@@ -0,0 +1,32 @@
+<template>
+<nav class="row">
+    <h1 class="col" id="logo">
+        <a href="/">
+            <img src="@/assets/images/logo.png" alt="project bunny">
+        </a>
+    </h1>
+    <ul class="col">
+        <li><a href="story">STORY</a></li>
+        <li><a href="story">BUNNY NFT</a></li>
+        <li><a href="story">ROADMAP</a></li>
+        <li><a href="story">TEAM</a></li>
+        <li><a href="story">PARTNER</a></li>
+        <li><a href="story">FAQ</a></li>
+    </ul>
+    <div class="col">
+        
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+nav {
+    background-color: rgba(0,0,0,0.8);
+}
+</style>
